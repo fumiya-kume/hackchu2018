7f332d1f4d58750102ff342fef84867589148307
Shinya Sakakibara <privater.net@gmail.com>
2018-02-23 13:27:07 +0900

    milkcocoa接続テスト

--- Scripts/milkcocoa.js
+++ Scripts/milkcocoa.js
@@ -5,7 +5,6 @@ window.onload = function(){
   var milkcocoa = MilkCocoa.connectWithApiKey('bluejdx9pwg5.mlkcca.com', 'PILCBGIODMIKICFL', 'iNVecMOSJGFmBTfOBKCffKRINJgADlfCVBlmPBKT');
   var ds = milkcocoa.dataStore('toothbrush');
 
-  // ds.on('send', changeViewFromSentMode);
 Å@ds.on('send', function() {
   alert('sendÇ≥ÇÍÇ‹ÇµÇΩÅI');
   });
